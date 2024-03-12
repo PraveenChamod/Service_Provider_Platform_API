f2069af138cad0ece2d6aeaf813e3177fb0d0d66
Praveen Chamod <praveen.c@fidenz.com>
2024-03-12 17:22:01 +0530

    Initially start the project

---
+++ src/main/java/com/ServiceProviderPlatform/Service_Provider_Platform_API/ServiceProviderPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.ServiceProviderPlatform.Service_Provider_Platform_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProviderPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ServiceProviderPlatform/Service_Provider_Platform_API/ServiceProviderPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiceProviderPlatform.Service_Provider_Platform_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
